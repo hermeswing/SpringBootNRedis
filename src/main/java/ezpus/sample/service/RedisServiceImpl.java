ee9a4d24a9e3fa890a4bf9ac54781cbfb764563a
hermeswing <hermeswing@naver.com>
2024-08-26 23:21:09 +0900

    주석변경 및 일부 소스 수정

--- src/main/java/ezpus/sample/service/RedisServiceImpl.java
+++ src/main/java/ezpus/sample/service/RedisServiceImpl.java
@@ -1,15 +1,14 @@
 package ezpus.sample.service;
 
-import java.util.Map;
-import java.util.Optional;
-
+import ezpus.sample.domain.Member;
+import ezpus.sample.repository.RedisRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ezpus.sample.domain.Member;
-import ezpus.sample.repository.RedisRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,11 +16,23 @@ public class RedisServiceImpl implements RedisService {
 
     private final RedisRepository redisRepository;
 
+    /**
+     * <pre>
+     *     value = "Member" :: "Member"라는 이름의 캐시에 메서드의 결과가 저장됩니다.
+     *     key = "#id" :: 캐시의 키를 지정합니다. id 파라미터 값이 캐시 키로 사용됩니다.
+     *     cacheManager = "cacheManager" :: "cacheManager"라는 이름의 캐시 매니저를 사용
+     *     unless = "#result == null" :: 메서드의 결과가 null인 경우 캐시에 저장되지 않도록 설정
+     * </pre>
+     * @Cacheable
+     * @param id
+     * @return
+     */
     @Override
     @Cacheable( value = "Member", key = "#id", cacheManager = "cacheManager", unless = "#result == null" )
     public Member getMember( Long id ) {
-        Optional<Member> member = redisRepository.findById( id );
-        return member.get();
+        // 해당 ID에 대한 Member 객체가 존재하지 않으면 예외가 발생하는 것을 방지 하기 위해
+        // .orElse(null)
+        return redisRepository.findById( id ).orElse( null );
     }
 
     @Override
@@ -37,14 +48,14 @@ public class RedisServiceImpl implements RedisService {
 
         Long id = 0L;
 
-        if ( param.get( "id" ) instanceof String ) {
+        if( param.get( "id" ) instanceof String ) {
             id = Long.parseLong( (String) param.get( "id" ) );
-        } else if ( param.get( "id" ) instanceof Integer ) {
+        } else if( param.get( "id" ) instanceof Integer ) {
             id = Long.valueOf( ( (Integer) param.get( "id" ) ).longValue() );
         }
 
-        Optional<Member> member     = redisRepository.findById( id );
-        Member           updateMember = member.get();
+        Optional<Member> member = redisRepository.findById( id );
+        Member updateMember = member.get();
         updateMember.updateMember( param );
 
         return redisRepository.save( updateMember );
